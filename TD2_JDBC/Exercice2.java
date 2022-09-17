593485d04f3776dd1949936797b22f4046d70dfb
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-09-17 19:34:21 +0200

    Ex2 show last element

--- TD2_JDBC/Exercice2.java
+++ TD2_JDBC/Exercice2.java
@@ -10,40 +10,58 @@ public class Exercice2
         Connection co = DriverManager.getConnection(url, args[0], args[1]);
         System.out.println("Successfully connected to the database :)");
 
-        Statement st = co.createStatement();
+        int type = ResultSet.TYPE_SCROLL_INSENSITIVE;
+        int mode = ResultSet.CONCUR_UPDATABLE;
+        Statement st = co.createStatement(type, mode);
 
-        ResultSet rS = st.executeQuery("SELECT * FROM empUL");
+
+
 
         /*
-        rS.next(); //show 1st element
+        //show 1st element
+        ResultSet rS = st.executeQuery("SELECT * FROM empUL");
+        rS.next();
+        show(rS);
+         */
+
+        //Show last element
+        ResultSet rS = st.executeQuery("SELECT * FROM empUL");
+        rS.last();
+        show(rS);
+
+        /*
+        while(rS.next()) {
+            if(rS.isLast()) {
+                show(rS);
+            }
+        }
+
+         */
+
+        /*
+        boolean suivant = true;
+        ResultSet rSTrouve = null;
+        while (rS != null)
+        {
+            rSTrouve = rS;
+            suivant = rS.next();
+            if (rS == null)
+                show(rSTrouve);
+        }
+                 */
+
+
+
+
+
+    }
+
+    public static void show(ResultSet rS) throws SQLException {
         System.out.println(
                 rS.getString(1) + "\t" +
                         rS.getString(2) + "\t" +
                         rS.getString(3) + "\t" +
                         rS.getString(4) + "\t" +
                         rS.getString(5) + "\t");
-        */
-
-        /*
-        boolean trouve = false;
-        /*
-        while (!trouve)
-        {
-            rSTrouve = rS;
-            if (!rS.next()) trouve = true;
-        }*/
-
-        /*
-        ResultSet rSTrouve = null;
-        do if (!trouve) rSTrouve = rS;
-        while (trouve = rS.next());
-
-        System.out.println(
-                rSTrouve.getString(1) + "\t" +
-                        rSTrouve.getString(2) + "\t" +
-                        rSTrouve.getString(3) + "\t" +
-                        rSTrouve.getString(4) + "\t" +
-                        rSTrouve.getString(5) + "\t");
-                     */
     }
 }
