495fa605af57106525dd26531dada9773cf2f151
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-09-21 12:01:15 +0200

    EDC Loc Q2

--- TP2_Location/src/Main.java
+++ TP2_Location/src/Main.java
@@ -7,22 +7,36 @@ public class Main
         AccessData ad = new AccessData(args[0], args[1]);
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please choose an action :\n1. List of available vehicles");
+        System.out.println("Please choose an action :\n1. List of available vehicles\n2. MAJ booking calendar");
         int choice = sc.nextInt();
         //int choice = 1;
         sc.nextLine();
 
+        String p1,p2,p3;
         switch (choice)
         {
             case 1:
                 System.out.print("--- List of available vehicles ---");
                 System.out.print("Category :");
-                String c = sc.nextLine();
+                p1 = sc.nextLine();
                 System.out.print("Start date (DD/MM/YYYY) :");
-                String stDate = sc.nextLine();
+                p2 = sc.nextLine();
                 System.out.print("End date (DD/MM/YYYY) :");
-                String endDate = sc.nextLine();
-                System.out.println(ad.listeVehic(c,stDate,endDate));
+                p3 = sc.nextLine();
+                System.out.println(ad.listeVehic(p1,p2,p3));
+                break;
+            case 2:
+                System.out.print("Start date (DD/MM/YYYY) :");
+                p1 = sc.nextLine();
+                System.out.print("End date (DD/MM/YYYY) :");
+                p2 = sc.nextLine();
+                System.out.print("Immat :");
+                p3 = sc.nextLine();
+                System.out.println(ad.majCal(p1, p2, p3));
+                ad.show();
+                break;
+            default:
+                break;
         }
     }
 
